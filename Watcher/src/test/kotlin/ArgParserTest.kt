45569ad5caee6c77baf776a7bb72686fe838c3db
vv-z84 <43834700+vv-z84@users.noreply.github.com>
2025-01-06 16:33:43 -0300

    [watcher] Add ArgParser

---
+++ Watcher/src/test/kotlin/ArgParserTest.kt
@@ -0,0 +1,24 @@
+import com.skubawa.doragon.watcher.ArgParser
+import org.junit.jupiter.api.assertThrows
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.expect
+
+class ArgParserTest {
+    private val argParser: ArgParser = ArgParser()
+
+    @Test
+    fun `test required argument --path`() {
+        val emptyArgs: Array<String> = arrayOf("")
+
+        assertThrows<IllegalArgumentException> {
+            argParser.parse(emptyArgs)
+        }
+
+        val emptyPathArgument: Array<String> = arrayOf("--path")
+
+        assertThrows<IllegalArgumentException> {
+            argParser.parse(emptyPathArgument)
+        }
+    }
+}

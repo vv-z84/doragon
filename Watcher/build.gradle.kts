2682d746c6301405fb090d6c3d3b6604d518c59d
vv-z84 <43834700+vv-z84@users.noreply.github.com>
2025-01-06 18:40:15 -0300

    [watcher] Install ktor client

--- Watcher/build.gradle.kts
+++ Watcher/build.gradle.kts
@@ -4,12 +4,17 @@ plugins {
 
 group = "com.skubawa.doragon.watcher"
 version = "1.0-SNAPSHOT"
+val ktor_version: String by project
+val logback_version: String by project
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation("io.ktor:ktor-client-core:$ktor_version")
+    implementation("io.ktor:ktor-client-cio:$ktor_version")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation(kotlin("test"))
 }
 

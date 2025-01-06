72560d394680f7146caa4ec21f77c52f641fb8e2
vv-z84 <43834700+vv-z84@users.noreply.github.com>
2025-01-06 16:45:26 -0300

    [watcher] Add FilepathValidator

---
+++ Watcher/src/main/kotlin/FilepathValidator.kt
@@ -0,0 +1,19 @@
+package com.skubawa.doragon.watcher
+
+import java.io.File
+
+class FilepathValidator {
+    fun validate(filepath: String): Boolean {
+        val file: File = File(filepath)
+
+        if(!file.exists()) {
+            throw RuntimeException("$filepath does not exists.")
+        }
+
+        if(!file.canRead()) {
+            throw RuntimeException("can't read $filepath")
+        }
+
+        return true
+    }
+}
---
+++ Watcher/src/test/kotlin/FilepathValidatorTest.kt
@@ -0,0 +1,35 @@
+import com.skubawa.doragon.watcher.FilepathValidator
+import org.junit.jupiter.api.assertThrows
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class FilepathValidatorTest {
+
+    private val validator: FilepathValidator = FilepathValidator()
+
+    @Test
+    fun `test inexistent filepath`() {
+        val invalidFilepath: String = "/notexists"
+
+        assertThrows<RuntimeException> {
+            validator.validate(invalidFilepath)
+        }
+    }
+
+    @Test
+    fun `test unreadable filepath`() {
+        val unreadableFilepath = "/root"
+
+        assertThrows<RuntimeException> {
+            validator.validate(unreadableFilepath)
+        }
+    }
+
+    @Test
+    fun `test readable filepath`() {
+        val readableFilepath = "/home"
+        val result: Boolean = validator.validate(readableFilepath)
+
+        assertTrue(result)
+    }
+}

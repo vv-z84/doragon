e9b5d994b1b127ff953c29cfb8d5e4ac073bc30f
vv-z84 <43834700+vv-z84@users.noreply.github.com>
2025-01-07 02:00:42 -0300

    [watcher] Implement Main.kt

--- Watcher/src/main/kotlin/Main.kt
+++ Watcher/src/main/kotlin/Main.kt
@@ -1,7 +1,31 @@
 package com.skubawa.doragon.watcher
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
+import com.skubawa.doragon.watcher.http.KtorHttpClientBuilder
+import com.skubawa.doragon.watcher.services.DoragonApiService
+import io.ktor.client.engine.cio.*
 
+val REQUIRED_ENV_VARS = listOf("DORAGON_API_BASE_URL")
+
+suspend fun main(args: Array<String>) {
+    validateEnvVars()
+    val argParser = ArgParser()
+    val (_, filepath) = argParser.parse(args)
+    val validFilepath = FilepathValidator().validate(filepath)
+
+    val watchService = WatchServiceBuilder().build(validFilepath.toString())
+
+    val doragonApiService = DoragonApiService(KtorHttpClientBuilder().build(CIO.create()))
+
+    val app = WatcherApp(validFilepath, watchService, doragonApiService)
+    app.run()
+}
+
+fun validateEnvVars() {
+    val env = System.getenv()
+    val missingEnvVars = REQUIRED_ENV_VARS.filterNot { envVar -> env.keys.contains(envVar) }
+
+    if(missingEnvVars.isNotEmpty()) {
+        println("Missing environment variables: ${missingEnvVars.joinToString(", ")}")
+        throw RuntimeException("Missing environment variables.")
+    }
 }

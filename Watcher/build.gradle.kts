3ad58afa9b2fcf4448c03abac04bb8f7218db919
vv-z84 <43834700+vv-z84@users.noreply.github.com>
2025-01-07 01:57:40 -0300

    [watcher] Install some libraries for serialization and configure project to be runnable

--- Watcher/build.gradle.kts
+++ Watcher/build.gradle.kts
@@ -1,5 +1,11 @@
 plugins {
     kotlin("jvm") version "2.0.21"
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.1.0"
+    application
+}
+
+application {
+    mainClass = "com.skubawa.doragon.watcher.MainKt"
 }
 
 group = "com.skubawa.doragon.watcher"
@@ -15,7 +21,11 @@ dependencies {
     implementation("io.ktor:ktor-client-core:$ktor_version")
     implementation("io.ktor:ktor-client-cio:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("io.ktor:ktor-client-logging:$ktor_version")
+    implementation("io.ktor:ktor-client-content-negotiation:$ktor_version")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
     testImplementation(kotlin("test"))
+    testImplementation("io.ktor:ktor-client-mock:$ktor_version")
 }
 
 tasks.test {
